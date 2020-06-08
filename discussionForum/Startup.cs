9d7910d38ee30c9755ef3e1e4495c2a0695dc0cb
Fatima Youssef <fatima.ysf.00@gmail.com>
2020-06-08 17:40:23 +0300

    First Commit

---
+++ discussionForum/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(discussionForum.Startup))]
+namespace discussionForum
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
